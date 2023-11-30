bdd98de23bbecbdf99d9aed76009aa10ac71f22f
HanSweet <2316186245@qq.com>
2023-11-30 19:30:43 +0800

    测试

--- src/text/hello.java
+++ src/text/hello.java
@@ -1,5 +1,10 @@
 package text;
 
 public class hello {
-
+public static void main(String[] args) {
+	System.out.println("helloGithub");
+	System.out.println("终于进去了");
+	System.out.println("终于进去了");
+	System.out.println("终于进去了");
+}
 }
